54c645de4bce63a17b9991eef21e1f9e2599016f
ri-ree <adrie270801@gmail.com>
2024-01-20 23:18:16 +0100

    Add event listener to the buttons
    An event listener was added so the display shows the value of the number clicked

--- calculator.js
+++ calculator.js
@@ -1,24 +1,36 @@
+const display = document.querySelector('#display');
+const content = document.createElement('div');
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+    content.textContent = button.id;
+    });
+});
+
+display.appendChild(content);
+
 function add(a, b) {
-    console.log(a + b);
+    return a + b;
 };
 
 function subtract(a, b) {
-    console.log (a - b);
+    return a - b;
 };
 
 function multiply(a, b) {
-    console.log(a * b);
+    return a * b;
 };
 
 function divide(a, b) {
-    console.log(a / b);
+    return a / b;
 };
 
 let firstNumber = 0;
 let operator = 0;
 let secondNumber = 0;
 
-function operator(operator, a, b) {
+function operate(a, operator, b) {
     if (operator == '+') {
         return add(a, b);
     } else if (operator == '-') {
